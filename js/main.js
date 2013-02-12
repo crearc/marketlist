fec497e2d7883eaa823bf9d5424108e24e84ad26
Eric Mills <ericmills2@gmail.com>
2013-02-12 00:01:30 -0600

    added animation, changed plus button

--- js/main.js
+++ js/main.js
@@ -3,13 +3,16 @@ $(document).ready(function(){
 	window.Stocks = new window.Stocker();
 
 	$('.folder-list li').live('click', function() {
-		console.log(Stocks.getFolder($(this).text()));
-		$('.stock-list').html(Stocks.getFolder($(this).text()).renderStocks());
+		var $that = $(this);
+		$('.stock-list').animate({ height: 'toggle', opacity: 'toggle' }, 'fast', function() {
+						$(this).html( Stocks.getFolder($that.text()).renderStocks() );
+						$(this).animate( { height: 'toggle', opacity: 'toggle' }, 'fast' );
+		});
 	});
 
 	$('.stock-list li .close').live('click', function() {
 		$(this).parent().animate({ height: 'toggle', opacity: 'toggle' }, 'fast');
-		Stocks.currFolder.removeTicker($(this).parent().children('.quote').text());
+		Stocks.currFolder.removeTicker( $(this).parent().children('.quote').text() );
 	});
 
 	$('.stock-list li').live('click', function() {
@@ -23,6 +26,7 @@ $(document).ready(function(){
 		}
 		var ticker = Stocks.currFolder.addTicker(item);
 		$('.stock-list').append(ticker.render());
+		.animate( { height: 'toggle', opacity: 'toggle' }, 'fast' );
 		//$('.close').first().parent().children('.quote').html()
 
 	});
